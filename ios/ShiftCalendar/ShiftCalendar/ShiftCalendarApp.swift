fd5fbba7daa7f2939cc945fd445444ca7a66a318
Lukas Berntsen <lukasberntsen1@gmail.com>
2025-12-08 22:27:33 +0100

    Initial ios commit

---
+++ ios/ShiftCalendar/ShiftCalendar/ShiftCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftCalendarApp.swift
+//  ShiftCalendar
+//
+//  Created by Lukas Berntsen on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShiftCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
